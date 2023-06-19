7188c6d55828f7555b7f237c8d053ea905c1afcf
Jomar Tigcal <jomar@tigcal.com>
2023-06-19 15:29:50 -0700

    Adding search view for searching Github users

--- app/src/main/java/com/tigcal/samples/githubr/ProfileActivity.kt
+++ app/src/main/java/com/tigcal/samples/githubr/ProfileActivity.kt
@@ -1,11 +1,41 @@
 package com.tigcal.samples.githubr
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.SearchManager
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+
+        val manager = getSystemService(SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.action_search)?.actionView as? SearchView
+        searchView?.let { searchVue ->
+            searchVue.setSearchableInfo(manager.getSearchableInfo(componentName))
+            searchVue.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    searchUser(query)
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    return true
+                }
+            })
+        }
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun searchUser(username: String) {
+        Log.d("search", "searching for $username")
     }
 }
