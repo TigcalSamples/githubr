095dc7a5e387cb932555c8fd374be7ac5cde84bb
Jomar Tigcal <jomar@tigcal.com>
2023-07-22 22:39:36 -0700

    Adding Github access token information

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ GitHubr
 ============================
 
 An Android app that displays GitHub user information, their followers, and people they follow.
+
+TODO:
+ - Create a Github Personal Access token and add it to [GithubrApp](app/src/main/java/com/tigcal/samples/githubr/GitHubrApp.kt)
--- app/src/main/java/com/tigcal/samples/githubr/GitHubrApp.kt
+++ app/src/main/java/com/tigcal/samples/githubr/GitHubrApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.squareup.moshi.Moshi
 import com.tigcal.samples.githubr.data.GitHubRepository
 import com.tigcal.samples.githubr.data.GitHubService
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -11,15 +12,29 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 class GitHubrApp: Application() {
     lateinit var gitHubRepository: GitHubRepository
 
+    private val token = "YOUR_PERSONAL_ACCESS_TOKEN"
+
     override fun onCreate() {
         super.onCreate()
 
         val moshi = Moshi.Builder()
             .build()
 
+        val okHttpClient = OkHttpClient.Builder().apply {
+            addInterceptor(
+                Interceptor { chain ->
+                    val builder = chain.request().newBuilder()
+                    builder.header("Authorization", "Bearer $token")
+                    return@Interceptor chain.proceed(builder.build())
+                }
+            )
+        }.build()
+
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com/")
             .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
             .build()
         val gitHubService = retrofit.create(GitHubService::class.java)
         gitHubRepository = GitHubRepository(gitHubService)
